a88cfacec80d33e6717e83fd5c0e3cd1c2cbde40
Till Mueller <metjm.mail@gmail.com>
2018-06-20 00:39:29 +0200

    vue webpack template

---
+++ ICOsim/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'ICOSim'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>

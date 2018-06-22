75bc40a6125de40c0db819c2c6bd41d5759d193a
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-06-21 20:33:24 -0700

    Working on leaderboards for the debug page.

--- ICOsim/src/logic/Game.js
+++ ICOsim/src/logic/Game.js
@@ -3,6 +3,49 @@ let settings = require("../static/settings.js");
 
 module.exports = 
 {
+    // TODO getICOLeaderboard
+    // TODO view someone elses ICO
+
+    getCoinMarketCaps(start_index, count, onSuccess, onError)
+    {
+        neb.nebRead("getCoinMarketCaps", null, onSuccess, onError);
+    },
+
+    getBestKnownScammers(start_index, count, onSuccess, onError)
+    {
+        neb.nebRead("getBestKnownScammers", null, onSuccess, onError);
+    },
+
+    getBuyPrice(item, quantity)
+    {
+        if(!quantity)
+        {
+            quantity = 1;
+        } else
+        {
+            quantity = parseInt(quantity);
+        }
+        var item_count = parseInt(item.user_holdings);
+        var max = item_count + quantity;
+        var multiple = (max * (max + 1) - item_count * (item_count + 1)) / 2;
+        return item.start_price * Math.pow(multiple, item.price_multiple);
+    },
+
+    getBuyPriceOld(item, quantity)
+    {
+        if(!quantity)
+        {
+            quantity = 1;
+        } else
+        {
+            quantity = parseInt(quantity);
+        }
+        var item_count = parseInt(item.user_holdings);
+        var max = item_count + quantity;
+        var multiple = (max * (max + 1) - item_count * (item_count + 1)) / 2;
+        return item.start_price * multiple;
+    },
+
     getInfo(onSuccess, onError)
     {
         neb.nebRead("getInfo", null, function(info) 
@@ -11,17 +54,19 @@ module.exports =
             {
                 return a.sort_id - b.sort_id;
             });
-            info.my_total_production_rate = info.my_production_rate * (1 + info.my_bonus / 100);
+            if(info.active_ico)
+            {
+                info.active_ico.my_total_production_rate = info.active_ico.my_production_rate * (1 + info.active_ico.my_bonus / 100);
+            }
             onSuccess(info);
         }, onError);
     },
 
     startICO(name, ticker, onTxPosted, onSuccess, onError)
     {
-        neb.nebWrite("createICO", [name, ticker], function(resp)
+        neb.nebWrite("launchICO", [name, ticker], function(resp)
         {
             onTxPosted(resp);
-            console.log(resp);
         }, 0, onSuccess, onError);
     },
 

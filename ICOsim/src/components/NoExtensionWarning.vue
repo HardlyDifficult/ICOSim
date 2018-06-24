d9795b87d9cc5b43e826e0411b68f255d2149968
Till Mueller <metjm.mail@gmail.com>
2018-06-24 03:37:49 +0200

    extension name

--- ICOsim/src/components/NoExtensionWarning.vue
+++ ICOsim/src/components/NoExtensionWarning.vue
@@ -5,7 +5,7 @@
         <div class="col-lg-6 warning-container glow-1">
         <h3>Hello! looks like you're new here.</h3>
             <p>
-                To play ICOSim you need the Nebulas <a target="_blank" class="highlight underline" href="https://chrome.google.com/webstore/detail/nasextwallet/gehjkhmhclgnkkhpfamakecfgakkfkco">Chrome Web Store</a> <a target="_blank" class="highlight underline" href="https://github.com/nebulasio/WebExtensionWallet">(Github)</a> and some <a target="_blank" class="highlight underline" href="http://givemenas.com/home.html">NAS</a>.<br>
+                To play ICOSim you need the Nebulas <a target="_blank" class="highlight underline" href="https://chrome.google.com/webstore/detail/nasextwallet/gehjkhmhclgnkkhpfamakecfgakkfkco">WebExtensionWallet</a> <a target="_blank" class="highlight underline" href="https://github.com/nebulasio/WebExtensionWallet">(Github)</a> and some <a target="_blank" class="highlight underline" href="http://givemenas.com/home.html">NAS</a>.<br>
                 No worries, you can get more than enough NAS to play for FREE from <a target="_blank" class="highlight underline" href="http://givemenas.com/home.html">givemenas.com</a><br>
                 <small>
                 NAS transactions are super cheap so even a fraction of a cent will allow you to play for weeks.<br>
